bd67120ac156e6279852e3383c26a009dc3442c7
wasimpatel-g10 <wasim.patel@g10consultancy.com>
2024-05-04 12:47:31 +0530

    master

---
+++ src/main/java/com/cicd/pipeline/PipelineApplication.java
@@ -0,0 +1,13 @@
+package com.cicd.pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelineApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/cicd/pipeline/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.cicd.pipeline.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+	@GetMapping("/welcome")
+	public String myMessage() {
+		return "hello world";
+	}
+	
+}
